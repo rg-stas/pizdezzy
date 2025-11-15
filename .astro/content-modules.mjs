d6c2fa3eee5bccae4edbb2fcd9e070173258df08
rg-stas <122228786+rg-stas@users.noreply.github.com>
2025-11-15 20:34:46 +0000

    added mdx support

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/content/home/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fhome%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   output : "static",
@@ -9,5 +11,7 @@ export default defineConfig({
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  integrations: [mdx()]
 });
